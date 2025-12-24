c8a2ccea3425bb01c3e033c85f70a0d905c396cd
KirillZ <zahvatinkirill@mail.ru>
2025-12-24 03:57:19 +0300

    feat: add application entry point and main activity

---
+++ app/src/main/kotlin/com/stoxmon/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.stoxmon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.stoxmon.ui.navigation.StoxmonNavigation
+import com.stoxmon.ui.theme.StoxmonTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StoxmonTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    StoxmonNavigation(navController = navController)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/kotlin/com/stoxmon/StoxmonApplication.kt
@@ -0,0 +1,7 @@
+package com.stoxmon
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StoxmonApplication : Application()
